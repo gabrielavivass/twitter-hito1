13812a60c811431120636c875938abd61c46df8d
Maria Gabriela Vivas <gabrielavivass13@gmail.com>
2021-05-11 19:45:44 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+    resources :tweets do 
+    resources :likes
+    member do
+      post 'retweet'
+    end
+  end
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+    } 
+  root "tweets#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
